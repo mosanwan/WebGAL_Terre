ff4be8cc399245a3a080f730ce52bde06b27bf24
墨三 <three@sanwu.ink>
2025-03-19 22:22:00 +0800

    阶段更新

--- packages/origine2/src/config/info.ts
+++ packages/origine2/src/config/info.ts
@@ -5,5 +5,5 @@ export interface Info {
 
 export const __INFO: Info = {
   version: '4.5.11',
-  buildTime: '2024-11-17T12:04:20.507Z', // 编译时会通过 version-sync.js 自动更新
+  buildTime: '2025-03-17T15:19:49.859Z', // 编译时会通过 version-sync.js 自动更新
 };
